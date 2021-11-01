af49a87e2abe36da370572d8f4879cb833c06d23
Noah Frederick <nfrederick023@gmail.com>
2021-10-31 22:05:21 -0400

    Update App.java

--- src/main/java/com/project0/App.java
+++ src/main/java/com/project0/App.java
@@ -2,9 +2,6 @@ package com.project0;
 
 import com.sql.SQL;
 
-/**
- * Hello world!
- */
 public final class App {
     public static void main(String[] args) throws Exception {
         SQL sql = new SQL();
